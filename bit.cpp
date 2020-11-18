e0081f54bc9fde7d110f72204a352e8219838a89
Fif0n <48093214+Fif0n@users.noreply.github.com>
2020-11-18 15:44:15 +0100

    Update bit.cpp

--- bit.cpp
+++ bit.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <bitset>
+#include <stdlib.h>
 using namespace std;
 
 string zerujBit(int liczba, int bit){
-	string num1 = bitset<8>(liczba & bit).to_string();
+	
+	int base = 0b11111111;
+	int fix = 0b00000001;
+	if(bit != 0){
+		fix <<=bit;
+	}	
+	base = base ^ fix;
+	string num1 = bitset<8>(liczba & base).to_string();
 	return num1;
 }
 
 int main(int argc, char** argv) {
-	cout<<zerujBit(0b0000001,0);
+	cout<<zerujBit(2,1);
 	return 0;
 }
